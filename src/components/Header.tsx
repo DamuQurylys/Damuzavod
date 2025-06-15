82d6360a325699280845c55419449aceb7d0a92e
DamuQurylys <sivakov.yuriy@gmail.com>
2025-06-15 16:07:47 +0500

    Плавное сжатие Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,17 @@ export default function Header() {
 
   useEffect(() => {
     const onScroll = () => {
-      setScrolled(window.scrollY > 20);
+      const currentScroll = window.scrollY;
+      if (currentScroll > 40 && !scrolled) {
+        setScrolled(true);
+      } else if (currentScroll < 10 && scrolled) {
+        setScrolled(false);
+      }
     };
+
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
-  }, []);
+  }, [scrolled]);
 
   return (
     <header
