414d7bae102221f4bcd74331849b237512543697
DamuQurylys <sivakov.yuriy@gmail.com>
2025-06-20 10:56:50 +0500

    Шрифт Anton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}', 
+    './src/**/*.{js,ts,jsx,tsx}', // захватывает все страницы и компоненты
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Основной шрифт по умолчанию для всего сайта
+        sans: ['Anton', 'sans-serif'],
+
+        // Альтернативный шрифт (если решим использовать Inter для текста)
+        inter: ['Inter', 'sans-serif'],
+
+        // Отдельно для заголовков, если понадобится сделать акцент
+        display: ['Anton', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
